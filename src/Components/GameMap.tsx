328208f36bd0a9491f20d7c1c89138d591e88498
cheoportu1960-glitch <cheoportu1960@gmail.com>
2026-08-15 22:33:39 -0400

    Refactor GameMap component syntax and style

--- src/Components/GameMap.tsx
+++ src/Components/GameMap.tsx
@@ -1,8 +1,9 @@
 
 import React from ' react';
-const GameMap : React.FC = (
-return
-<div style={{ width: '100%'  height:100% }} <h2>Mapa del Juego Listo</h2>
+const GameMap : React.FC = () =>{
+return (
+<div style={{ width: '100%',  height: '100%' }}><h2> del juego</h2>
 </div>
 );
+  };
 export default GameMap;
